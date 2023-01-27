20732ffbf883eb5c8f4111b62c1658c2df363893
Hurtado1998 <50411890+Hurtado1998@users.noreply.github.com>
2023-01-27 17:42:45 -0500

    Add files via upload

---
+++ Javascript/Ejercicio1_Nuevo.js
@@ -0,0 +1,40 @@
+/*
+DAACTM
+1.Definition: Programa que sume 2 numeros y devuelva resultado.
+2.Analisis: Se deben crear 3 variables y un metodo imprimir
+-Variable num1, Variable num2 que son las que se suman.
+-Variable resultado que es la suma de num1 y num2.
+-Metodo alert() que imprima el resultado
+
+3.Algorithm
+-1.Variables num1 y num2 ingresan los valores
+-Variable resultado realiza la operacion suma
+-Alert imprime el resultado
+
+4.Code 
+*/
+
+let a,b,c;
+a=1,b=20111,c = a + b;
+alert("El resultado es " + c);
+
+/*
+5.Testing and debugging
+-Debugging: 
+1)Se redujo el codigo al crear varias variables en la misma linea y no en diferentes.
+
+-Testing
+1)Suma 10    APROBADO
+2)Suma 100   APROBADO
+3)Suma 1000  APROBADO
+4)Suma 10000 APROBADO
+
+-MANDAR A PRODUCCION: APROBADO 
+
+
+6.Maintenance
+-Usuario, Herramientas nuevas en el mercado
+
+-Usuario: A la espera de comentarios por parte del usuario
+-Mercado: En busqueda y espera de nuevas tecnologias.
+*/
